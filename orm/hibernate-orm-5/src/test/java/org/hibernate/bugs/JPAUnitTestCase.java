ec255ef9cdb5931c88f081d63195b7543fc22d55
douph1 <45632394+douph1@users.noreply.github.com>
2022-09-14 10:07:04 +0200

    Add Read test

--- orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
+++ orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/JPAUnitTestCase.java
@@ -12,6 +12,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.nio.ByteBuffer;
 import java.util.UUID;
 
 import static org.junit.Assert.assertEquals;
@@ -41,7 +42,7 @@ public class JPAUnitTestCase {
 	//The default UUID mapping is the binary one because it uses a more efficient column storage.
 	//Probably break by this commit with VARCHAR (always) : https://github.com/hibernate/hibernate-orm/pull/4860/commits/0ea2c11dff943ae8063fd355d4e7fd6cff9e6a92
 	@Test
-	public void HHH15097Test() throws Exception {
+	public void HHH15097TestInsert() throws Exception {
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
 
@@ -52,10 +53,34 @@ public class JPAUnitTestCase {
 		Integer newId = e.getId();
 		entityManager.getTransaction().commit();
 
-
-		// retrieve the record and verify values.
+		// retrieve the record and verify values ... if insert success.
 		final TestEntity e2 = entityManager.find( TestEntity.class, newId );
 		assertEquals( uuid, e2.getSomeValue().getUuid() );
+	}
+
+
+	//UUIDConverter allow to convert binary(16) to UUID in wrapped object SomeValue
+	@Test
+	public void HHH15097TestRead() {
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		entityManager.getTransaction().begin();
+
+		//insert with native query to bypass bug
+		entityManager.createNativeQuery("insert into TestEntity (id, someValue) values (?, ?)")
+				.setParameter(1, "1")
+				.setParameter(2, convertUUIDToBytes(UUID.fromString("468c0ef1-243a-4fff-817a-645ab4078bb3")))
+				.executeUpdate();
+		entityManager.getTransaction().commit();
+
+		assertEquals(UUID.fromString("468c0ef1-243a-4fff-817a-645ab4078bb3"),entityManager.find( TestEntity.class, 1 ).getSomeValue().getUuid());
+
+	}
+
+	private static byte[] convertUUIDToBytes(UUID uuid) {
+		ByteBuffer bb = ByteBuffer.wrap(new byte[16]);
+		bb.putLong(uuid.getMostSignificantBits());
+		bb.putLong(uuid.getLeastSignificantBits());
+		return bb.array();
 
 	}
 }
